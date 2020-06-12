eeafdf5cfa9baca758fdc3927508a5b3587545b6
Joshua Peavler <joshuaapeavler@gmail.com>
2020-06-12 14:52:20 -0600

    Completed Resistor Color exercism in C#

---
+++ resistor-color/ResistorColor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public static class ResistorColor
+{
+    public static int ColorCode(string color)
+    {
+        int index = 10;
+        string[] colorArray = Colors();
+        for(int i = 0; i < colorArray.Length; i++) {
+            if(colorArray[i] == color){index = i;}
+        }
+        return index;
+    }
+
+    public static string[] Colors()
+    {
+        return new [] {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+    }
+}
