f6244fde304668a3c489bb5f23452c781c758ccf
Joshua Peavler <joshuaapeavler@gmail.com>
2020-07-30 13:57:31 -0600

    Added do while loop to name generation code to ensure only new robot names were made

--- robot-name/RobotName.cs
+++ robot-name/RobotName.cs
@@ -1,16 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 public class Robot {
     private string generateName () {
-        var random = new Random();
-        string letter1 = ALPHABET[random.Next(26)].ToString();
-        string letter2 = ALPHABET[random.Next(26)].ToString();
-        string num1 = random.Next(10).ToString();
-        string num2 = random.Next(10).ToString();
-        string num3 = random.Next(10).ToString();
-        return string.Concat(letter1, letter2, num1, num2, num3);
+        string name = string.Empty;
+        do {
+            var random = new Random();
+            string letter1 = ALPHABET[random.Next(26)].ToString();
+            string letter2 = ALPHABET[random.Next(26)].ToString();
+            string num1 = random.Next(10).ToString();
+            string num2 = random.Next(10).ToString();
+            string num3 = random.Next(10).ToString();
+            name = string.Concat(letter1, letter2, num1, num2, num3);
+        } while(!usedNames.Add(name));
+        return name;
     }
     private string robotName;
+    private static HashSet <string> usedNames = new HashSet<string>();
     private string ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public Robot() {
         robotName = generateName();
