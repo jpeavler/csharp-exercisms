7924bf93a10ae722e171e963b3d4425d5ddcf02a
Joshua Peavler <joshuaapeavler@gmail.com>
2020-07-30 11:13:03 -0600

    Completed C# NucleotideCount exercism

--- nucleotide-count/NucleotideCount.cs
+++ nucleotide-count/NucleotideCount.cs
@@ -5,12 +5,24 @@ public static class NucleotideCount
 {
     public static IDictionary<char, int> Count(string sequence)
     {
-        var nucleoCount = new Dictionary<char, int> {
-            ['A'] = 0,
-            ['C'] = 0,
-            ['G'] = 0,
-            ['T'] = 0
-        };
+        int aCount = 0;
+        int cCount = 0;
+        int gCount = 0;
+        int tCount = 0;
+        foreach (char c in sequence) {
+            if(c == 'A') { aCount++;}
+            else if (c == 'C') { cCount++;}
+            else if (c == 'G') { gCount++;}
+            else if (c == 'T') { tCount++;}
+            else {throw new System.ArgumentException("Includes invalid Characters");}
+        }
+        var nucleoCount = new Dictionary<char, int>();
+        nucleoCount.Add('A', aCount); 
+        nucleoCount.Add('C', cCount); 
+        nucleoCount.Add('G', gCount); 
+        nucleoCount.Add('T', tCount); 
+        
+
         return nucleoCount;
     }
 }
